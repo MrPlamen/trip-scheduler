9af4fdbffbb3a3400d4bb245598597341a789f63
Plamen Petrov <plamen.veselinov.pterov@gmail.com>
2025-03-20 08:08:01 +0200

    set the body css

--- client/src/components/trip-create/TripCreate.jsx
+++ client/src/components/trip-create/TripCreate.jsx
@@ -24,7 +24,7 @@ export default function TripCreate() {
 
                     <label htmlFor="summary">Summary:</label>
                     <textarea name="summary" id="summary"></textarea>
-                    <input className="btn submit" type="submit" value="Create Game" />
+                    <input className="btn submit" type="submit" value="Create" />
                 </div>
             </form>
         </section>
