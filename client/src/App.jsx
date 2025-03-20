2f8b57b17cb1ae55fb43de8631241772182e1941
Plamen Petrov <plamen.veselinov.pterov@gmail.com>
2025-03-20 23:47:02 +0200

    started a catalog page html

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/home/Home'
 import TripCreate from './components/trip-create/TripCreate'
 import Register from './components/register/Register'
 import Login from './components/login/Login'
+import TripCatalog from './components/trip-catalog/TripCatalog'
 import './App.css'
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path='/trips/create' element={<TripCreate />} />
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/trips' element={<TripCatalog />} />
         </Routes>
       </main>
     </div>
---
+++ client/src/components/trip-catalog/TripCatalog.jsx
@@ -0,0 +1,12 @@
+import TripCatalogItem from "./trip-catalog-item/TripCatalogItem";
+
+export default function TripCatalog() {
+
+    return (
+        <section id="catalog-page">
+            <h1>All trips</h1>
+            
+             <h3 className="no-articles">No articles yet</h3>
+        </section>
+    );
+}
