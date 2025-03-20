4808f46368f7102297b2d7091d9aa1c7b6a3d3d8
Plamen Petrov <plamen.veselinov.pterov@gmail.com>
2025-03-20 21:42:55 +0200

    added register page/route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
 import TripCreate from './components/trip-create/TripCreate'
+import Register from './components/register/Register'
 import './App.css'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/trips/create' element={<TripCreate />} />
+          <Route path='/register' element={<Register />} />
         </Routes>
       </main>
     </div>
