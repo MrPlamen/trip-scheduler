8bbcd831a6cbd01e216ca78a154cc54c959633cb
Plamen Petrov <plamen.veselinov.pterov@gmail.com>
2025-03-20 22:12:12 +0200

    added a login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/header/Header'
 import Home from './components/home/Home'
 import TripCreate from './components/trip-create/TripCreate'
 import Register from './components/register/Register'
+import Login from './components/login/Login'
 import './App.css'
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/trips/create' element={<TripCreate />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </main>
     </div>
