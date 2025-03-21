8698989d2c00a52387dc771b99bd39d9b263d672
Plamen Petrov <plamen.veselinov.pterov@gmail.com>
2025-03-21 23:03:33 +0200

    catalog page structure

--- client/src/components/trip-catalog/TripCatalog.jsx
+++ client/src/components/trip-catalog/TripCatalog.jsx
@@ -5,8 +5,34 @@ export default function TripCatalog() {
     return (
         <section id="catalog-page">
             <h1>All trips</h1>
-            
-             <h3 className="no-articles">No articles yet</h3>
+            <div class="allGames">
+                <div class="allGames-info">
+                    <img src="/images/avatar-1.jpg" />
+                    <h6>Spain</h6>
+                    <h2>May</h2>
+                    <a href="#" class="details-button">Details</a>
+                </div>
+
+            </div>
+            <div class="allGames">
+                <div class="allGames-info">
+                    <img src="/images/avatar-1.jpg" />
+                    <h6>Baba mi</h6>
+                    <h2>July</h2>
+                    <a href="#" class="details-button">Details</a>
+                </div>
+
+            </div>
+            <div class="allGames">
+                <div class="allGames-info">
+                    <img src="/images/avatar-1.jpg" />
+                    <h6>Greece</h6>
+                    <h2>August</h2>
+                    <a href="#" class="details-button">Details</a>
+                </div>
+            </div>
+
+            <h3 class="no-articles">No articles yet</h3>
         </section>
     );
 }
