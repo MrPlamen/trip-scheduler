8b469e83b96eeb8492014b8e0f81ebea94a66d66
Plamen Petrov <plamen.veselinov.pterov@gmail.com>
2025-03-22 18:31:28 +0200

    All trips screen works dynamically

--- client/src/components/trip-catalog/TripCatalog.jsx
+++ client/src/components/trip-catalog/TripCatalog.jsx
@@ -16,35 +16,12 @@ export default function TripCatalog() {
 
     return (
         <section id="catalog-page">
-            <h1>All trips</h1>
-            <div class="allGames">
-                <div class="allGames-info">
-                    <img src="/images/avatar-1.jpg" />
-                    <h6>Spain</h6>
-                    <h2>May</h2>
-                    <a href="#" class="details-button">Details</a>
-                </div>
+            <h1>All Trips</h1>
 
-            </div>
-            <div class="allGames">
-                <div class="allGames-info">
-                    <img src="/images/avatar-1.jpg" />
-                    <h6>England</h6>
-                    <h2>July</h2>
-                    <a href="#" class="details-button">Details</a>
-                </div>
-
-            </div>
-            <div class="allGames">
-                <div class="allGames-info">
-                    <img src="/images/avatar-1.jpg" />
-                    <h6>Greece</h6>
-                    <h2>August</h2>
-                    <a href="#" class="details-button">Details</a>
-                </div>
-            </div>
-
-            <h3 class="no-articles">No articles yet</h3>
+            {trips.length > 0
+                ? trips.map(trip => <TripCatalogItem key={trip._id} {...trip} />)
+                : <h3 className="no-articles">No articles yet</h3>
+            }
         </section>
     );
 }
