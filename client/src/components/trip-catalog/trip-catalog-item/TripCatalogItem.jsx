fcc6719e04b1b979242a15283910c20fa3cd0a6e
Plamen Petrov <plamen.veselinov.pterov@gmail.com>
2025-03-22 18:50:28 +0200

    fixed all trips card contents

--- client/src/components/trip-catalog/trip-catalog-item/TripCatalogItem.jsx
+++ client/src/components/trip-catalog/trip-catalog-item/TripCatalogItem.jsx
@@ -5,13 +5,17 @@ export default function TripCatalogItem({
     title,
     category,
     imageUrl,
+    length,
+    summary
 }) {
     return (
         <div className="allGames">
             <div className="allGames-info">
                 <img src={imageUrl} />
-                <h6>{category}</h6>
                 <h2>{title}</h2>
+                <h6>{category}</h6>
+                <h6>{`${length} days`}</h6>
+                <p>{summary}</p>
                 <Link to={`/trips/${_id}/details`} className="details-button">Details</Link>
             </div>
         </div>
