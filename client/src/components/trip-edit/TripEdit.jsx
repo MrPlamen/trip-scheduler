89cf287d433011201870f2a99917b5a8d4d8f561
Plamen Petrov <plamen.veselinov.pterov@gmail.com>
2025-03-23 21:49:42 +0200

    state values appearing on edit

--- client/src/components/trip-edit/TripEdit.jsx
+++ client/src/components/trip-edit/TripEdit.jsx
@@ -1,8 +1,16 @@
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
+import tripService from "../../services/tripService";
 
 export default function TripEdit() {
     const navigate = useNavigate();
     const { tripId } = useParams();
+    const [trip, setTrip] = useState({});
+
+    useEffect(() => {
+        tripService.getOne(tripId)
+        .then(setTrip);
+    }, [tripId]);
 
     return (
         <section id="edit-page" className="auth">
@@ -11,20 +19,20 @@ export default function TripEdit() {
 
                     <h1>Edit trip</h1>
                     <label htmlFor="leg-title">Legendary title:</label>
-                    <input type="text" id="title" name="title" value="" />
+                    <input type="text" id="title" name="title" defaultValue={trip.title} />
 
                     <label htmlFor="category">Category:</label>
-                    <input type="text" id="category" name="category" value="" />
+                    <input type="text" id="category" name="category" defaultValue={trip.category} />
 
                     <label htmlFor="duration">MaxLevel:</label>
-                    <input type="number" id="duration" name="duration" min="1" value="" />
+                    <input type="number" id="duration" name="duration" min="1" defaultValue={trip.duration} />
 
                     <label htmlFor="game-img">Image:</label>
-                    <input type="text" id="imageUrl" name="imageUrl" value="" />
+                    <input type="text" id="imageUrl" name="imageUrl" defaultValue={trip.imageUrl} />
 
                     <label htmlFor="summary">Summary:</label>
-                    <textarea name="summary" id="summary"></textarea>
-                    <input className="btn submit" type="submit" value="Edit Game" />
+                    <textarea name="summary" id="summary" defaultValue={trip.summary}></textarea>
+                    <input className="btn submit" type="submit" defaultValue="Edit trip" />
 
                 </div>
             </form>
