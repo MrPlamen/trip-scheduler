cbb0e7ee90868f2bd73e1bc5b8b7e164ec17f725
Plamen Petrov <plamen.veselinov.pterov@gmail.com>
2025-03-26 23:41:26 +0200

    redirect after logout

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,24 +1,25 @@
-import { UserContext } from "../contexts/UserContext";
+import { useEffect } from "react";
+import { useContext } from "react";
 import request from "../utils/request"
+import { UserContext } from "../contexts/UserContext";
 
-const baseUrl = 'http://localhost:3030/users'
+const baseUrl = 'http://localhost:3030/users';
 
 export const useLogin = () => {
+    const login = async (email, password) =>
+        request.post(
+            `${baseUrl}/login`,
+            { email, password },
+        );
 
-    const login = async (email, password) => {
-        const result = request.post(`${baseUrl}/login`, { email, password});
-
-        return result;
-    }
     return {
         login,
     }
 };
 
 export const useRegister = () => {
-    const register = (email, password) => {
-        return request.post(`${baseUrl}/register`, { email, password })
-    };
+    const register = (email, password) =>
+        request.post(`${baseUrl}/register`, { email, password });
 
     return {
         register,
