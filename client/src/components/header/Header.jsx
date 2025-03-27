f2c0cf39512c8131712182bfd9306e4f48158b5b
Plamen Petrov <plamen.veselinov.pterov@gmail.com>
2025-03-27 21:52:41 +0200

    hide login and register for logged

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,17 +1,28 @@
+import { useContext } from "react";
 import { Link } from "react-router";
+import { UserContext } from "../../contexts/UserContext";
 
 export default function Header() {
+    const { email } = useContext(UserContext);
+
     return (
         <header>
             <h1><Link className="hero-home" to="/">Trip Planner</Link></h1>
             <nav className="hero-nav">
                 <Link to="/trips">All trips</Link>
 
-                <Link to="/trips/create">Create trip</Link>
-                <Link to="/logout">Logout</Link>
-
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
+                {email ? (
+                    <>
+                        <Link to="/trips/create">Create trip</Link>
+                        <Link to="/logout">Logout</Link>
+                    </>
+                )
+                : (
+                     <>
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </>
+                )}
 
             </nav>
         </header>
