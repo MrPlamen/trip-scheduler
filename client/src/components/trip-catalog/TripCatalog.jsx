42959d0e54b8a7f04db1801e3fc39cbf2cd227df
Plamen Petrov <plamen.veselinov.pterov@gmail.com>
2025-04-02 23:48:57 +0300

    successfully filter trips by member

--- client/src/components/trip-catalog/TripCatalog.jsx
+++ client/src/components/trip-catalog/TripCatalog.jsx
@@ -1,16 +1,21 @@
+import { useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";  
 import TripCatalogItem from "./trip-catalog-item/TripCatalogItem";
 import { useTrips } from "../../api/tripApi";
 
 export default function TripCatalog() {
-    const { trips } = useTrips();
+    const { trips } = useTrips(); 
+    const { email } = useContext(UserContext);  
+
+    const userTrips = trips.filter(trip => Array.isArray(trip.members) && trip.members.includes(email));
 
     return (
         <section id="catalog-page">
-            <h1>All Trips</h1>
+            <h1>Trips You Are a Part Of</h1>
 
-            {trips.length > 0
-                ? trips.map(trip => <TripCatalogItem key={trip._id} {...trip} />)
-                : <h3 className="no-articles">No trips planned yet</h3>
+            {userTrips.length > 0
+                ? userTrips.map(trip => <TripCatalogItem key={trip._id} {...trip} />)
+                : <h3 className="no-articles">You are not a member of any trips yet.</h3>
             }
         </section>
     );
