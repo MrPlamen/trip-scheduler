b4fe6b60c1c846169f8e6508379a9324fbe1cb05
MrPlamen <blackmore.ritchie.blackmore@gmail.com>
2025-04-05 12:10:09 +0300

    fixed all visit points cards

--- client/src/components/item-catalog/VisitItemCatalog.jsx
+++ client/src/components/item-catalog/VisitItemCatalog.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";  
 import { useVisitItems } from "../../api/visitItemApi";
-import TripCatalogItem from "../trip-catalog/trip-catalog-item/TripCatalogItem";
+import VisitItemCatalogItem from "./visititem-catalog-item/VisitItemCatalogItem";
 
 export default function VisitItemCatalog() {
     const { visitItems } = useVisitItems(); 
@@ -16,7 +16,7 @@ export default function VisitItemCatalog() {
             <h1>Visit points you are a part of</h1>
 
             {userItems.length > 0
-                ? userItems.map(visitItem => <TripCatalogItem key={visitItem._id} {...visitItem} />)
+                ? userItems.map(visitItem => <VisitItemCatalogItem key={visitItem._id} {...visitItem} />)
                 : <h3 className="no-articles">You are not a member of any points for visit yet.</h3>
             }
         </section>
