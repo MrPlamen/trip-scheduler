7e737ad306cf1bf7c2ac6f4fa04a1529dc04ae11
MrPlamen <blackmore.ritchie.blackmore@gmail.com>
2025-04-05 17:21:41 +0300

    visit point details page updated

--- client/src/components/item-details/VisitItemDetails.jsx
+++ client/src/components/item-details/VisitItemDetails.jsx
@@ -1,28 +1,20 @@
 import { useParams } from "react-router";
-import { useVisitItem } from "../../api/visitItemApi"; // Updated hook
+import { useVisitItem } from "../../api/visitItemApi";
 
 export default function VisitItemDetails() {
     const { visitItemId } = useParams();
     const { visitItem, isLoading, error } = useVisitItem(visitItemId);
 
-    // Log for debugging purposes
     console.log(`params id: ${visitItemId}`);
     console.log(`visit point: ${visitItem}`);
 
-    // Handle loading state
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-
-    // Handle error state
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
-
-    // Handle no visitItem found
     if (!visitItem) {
         return <div>Visit item not found!</div>;
     }
+
+    const timestamp = visitItem._createdOn;
+
+    const date = new Date(timestamp);
 
     return (
         <section id="trip-details">
@@ -31,7 +23,7 @@ export default function VisitItemDetails() {
                 <div className="trip-header">
                     <img className="trip-img" src={visitItem.imageUrl} alt={visitItem.title} />
                     <h1>{visitItem.title}</h1>
-                    <span className="levels">Duration: {visitItem.duration}</span>
+                    <span className="levels">Created on: {date.toLocaleDateString()}</span>
                     <p className="type">{visitItem.category}</p>
                 </div>
 
